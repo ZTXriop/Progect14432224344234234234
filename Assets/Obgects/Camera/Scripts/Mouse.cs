ab4f90289166a947cef9897e2df6fa37835c60e1
ZTXriop <158745310+ZTXriop@users.noreply.github.com>
2025-01-10 21:21:41 +0300

    Засейвить бы...

--- Assets/Obgects/Camera/Scripts/Mouse.cs
+++ Assets/Obgects/Camera/Scripts/Mouse.cs
@@ -6,15 +6,15 @@ public class Mouse : MonoBehaviour
 {
     public GameObject ykasatel;
     private Camera cam;
+
     void Start()
     {
-        
         cam = Camera.main;
+        
     }
     
     void Update()
     {
-        
         if (Input.GetMouseButtonDown(1))
         {
             // сюда запишется инфо о пересечении луча, если оно будет
@@ -29,6 +29,19 @@ public class Mouse : MonoBehaviour
                 // Если пересечение есть, можно получить информацию о нём
                 Debug.Log("Попал в объект: ");
                 GameObject newObject = Instantiate(ykasatel, hit.point, Quaternion.identity);
+            }
+        }
+        if (Input.GetMouseButtonDown(0))
+        {
+            // сюда запишется инфо о пересечении луча, если оно будет
+            RaycastHit hit;
+            // сам луч, начинается от позиции камеры походу и направлен в сторону курсора
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            // пускаем луч
+            Physics.Raycast(ray, out hit);
+            if (Physics.Raycast(ray, out hit))
+            {
+                
             }
         }
     }
