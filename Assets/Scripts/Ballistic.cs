18c56dacbcda1d4d0d901fec46479d69976ce648
ZTXriop <158745310+ZTXriop@users.noreply.github.com>
2025-01-20 15:51:26 +0300

    Чисто поворот башни у т 90 сделал к точке)))

---
+++ Assets/Scripts/Ballistic.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ballistic : MonoBehaviour
+{
+    public Transform SpawnTransform;
+    public Transform TargetTransform;
+
+    public float AngleInDegress;
+    void Start()
+    {
+        
+    }
+    void Update()
+    {
+        Vector3 fromTo = TargetTransform.position - transform.position;
+        Vector3 fromToXZ = new Vector3(fromTo.x, 0f,  fromTo.z);
+        transform.rotation = Quaternion.LookRotation(fromToXZ, Vector3.up);
+
+    }
+}
