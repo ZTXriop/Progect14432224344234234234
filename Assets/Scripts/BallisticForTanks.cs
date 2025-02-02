2b54ddb80fc37b5953a15c868bf53837e8647cea
ZTXriop <158745310+ZTXriop@users.noreply.github.com>
2025-02-02 11:55:40 +0300

    Половина механики разведки у юнита
    теперь юнит ищет все юниты со скриптом в радиусе, а также проверяет видены ли они для объекта, осталось сделать так чтобы юнит ставил позицию стрельбы и эта ебля наконец то закончится))))))

--- Assets/Scripts/BallisticForTanks.cs
+++ Assets/Scripts/BallisticForTanks.cs
@@ -11,6 +11,7 @@ public class BallisticForTanks : MonoBehaviour
 
     public float AngleInDegress;
     public float speed;
+    public float radius = 5.0f;
 
     public bool noticed = false;
 
@@ -20,14 +21,38 @@ public class BallisticForTanks : MonoBehaviour
     }
     void Update()
     {
+        ///////////////////////////////////////Поворот башни///////////////////////////////////////
         var rotation = Quaternion.LookRotation(TargetTransform.position - transform.position);
         transform.rotation = Quaternion.Slerp(transform.rotation, rotation, Time.deltaTime * speed);
-        
+        //////////////////////////////////Обнаружение противника///////////////////////////////////
+        Vector3 explosionPosition = transform.position;//Центр объекта
+        Collider[] colliders = Physics.OverlapSphere(explosionPosition, radius);//Массив всех объектов в радиусе
+        foreach (Collider hit in colliders) //Проверяем все объекты последовательно
+        {
+            if (hit.transform.GetComponent<UnitInfo>())
+            {
+                Debug.Log("Вижу");
+                RaycastHit hit2;
+                Ray ray = new Ray(transform.position, hit.transform.position - transform.position);
+                Physics.Raycast(ray, out hit2);
+                if (hit2.collider != null)
+                {
+                    if (hit2.collider.gameObject != hit.gameObject)
+                    {
+                        Debug.Log("Путь к врагу преграждает объект: " + hit2.collider.name);
+                    }
+                    else
+                    {
+                        Debug.Log("Попадаю во врага!!!");
+                    }
+                }
+            }
+        }
         if (noticed == true) 
         {
 
         }
-        //Пока пусть по нажатию кнопки будет, потом надо сделать по дистанции!!!!!
+        //Стрельба. Пока пусть по нажатию кнопки будет, потом надо сделать по дистанции!!!!!
         if (Input.GetMouseButtonDown(0))
         {
             Shot();
@@ -38,5 +63,8 @@ public class BallisticForTanks : MonoBehaviour
     {
         GameObject newBullet = Instantiate(Bullet, SpawnTransform.position, SpawnTransform.rotation);
     }
-    
+    public void Proverka()
+    {
+
+    }
 }
